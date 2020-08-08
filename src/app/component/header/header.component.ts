77514d54b16ec53bfb6d745222be375c9e918dc5
Patricia <patricia.warmelink@outlook.de>
2020-08-08 22:41:07 +0200

    header update

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { User } from '../../model/user'
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  public user: User;
+  public userDataVisible: boolean;
+
+  constructor() {
+    this.userDataVisible = false;
+    this.user = {
+      firstName: 'Max',
+      lastName: 'Mustermann',
+      email: 'max@mustermann.de',
+      institution: 'FZ Jülich',
+      orcid: '4711'
+    };
+  }
 
   ngOnInit(): void {
+  }
+
+  public saveUserData(): void {
+    this.hideUserData();
+  }
+
+  public showUserData(): void {
+    this.userDataVisible = true;
+  }
+
+  hideUserData(): void {
+    this.userDataVisible = false;
   }
 
 }
