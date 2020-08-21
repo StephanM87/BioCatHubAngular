c26e679adc373ac902d9a22f10216b259efb8c00
Patricia <patricia.warmelink@outlook.de>
2020-08-21 13:15:44 +0200

    Excel Datei einlesen

--- src/app/component/measurement/measurement.component.ts
+++ src/app/component/measurement/measurement.component.ts
@@ -14,6 +14,10 @@ export class MeasurementComponent implements OnInit {
   public vessel: Vessel;
   public file: File;
 
+  public measurementTable: boolean;
+  public measurementHeader: Array<string>;
+  public measurement: Array<Object>;
+
   constructor(public dataService: DataService) {
     this.vessel = dataService.getExperiment().getVessel();
    }
@@ -30,21 +34,31 @@ export class MeasurementComponent implements OnInit {
   }
 
   public uploadFile(): void {
-    const reader: FileReader = new FileReader();
-    reader.readAsBinaryString(this.file);
-    reader.onload = (e: any) => {
-      /* create workbook */
-      const binarystr: string = e.target.result;
-      const wb: XLSX.WorkBook = XLSX.read(binarystr, { type: 'binary' });
+    if(this.file != undefined){
+      this.measurement = new Array();
 
-      /* selected the first sheet */
-      const wsname: string = wb.SheetNames[0];
-      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
-
-      /* save data */
-      const data = XLSX.utils.sheet_to_json(ws); // to get 2d array pass 2nd parameter as object {header: 1}
-      console.log(data); // Data will be logged in array format containing objects
-    };
+      const reader: FileReader = new FileReader();
+      reader.readAsBinaryString(this.file);
+      reader.onload = (e: any) => {
+        /* create workbook */
+        const binarystr: string = e.target.result;
+        const wb: XLSX.WorkBook = XLSX.read(binarystr, { type: 'binary' });
+  
+        /* selected the first sheet */
+        const wsname: string = wb.SheetNames[0];
+        const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+  
+        /* save data */
+        const data = XLSX.utils.sheet_to_json(ws);
+        data.forEach(element => {
+          this.measurement.push(element);
+        });
+        if(this.measurement.length > 0){
+          this.measurementHeader = Object.keys(this.measurement[0]);
+          this.measurementTable = true;
+        }
+      };
+    }
   }
 
 }
