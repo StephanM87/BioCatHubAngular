0c0a39b6e5aacf0f61ebc26471d3782f30e5994f
LohmannJens <jens.johann@outlook.de>
2021-03-29 17:49:17 +0200

    Updated Angular to V11.2.7

--- src/app/component/vessel/vessel.component.spec.ts
+++ src/app/component/vessel/vessel.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { VesselComponent } from './vessel.component';
 
@@ -6,7 +6,7 @@ describe('VesselComponent', () => {
   let component: VesselComponent;
   let fixture: ComponentFixture<VesselComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ VesselComponent ]
     })
